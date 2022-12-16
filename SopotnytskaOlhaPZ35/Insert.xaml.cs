7dc8aef9b8561e60f6c76d955a004a585ca00e0d
olha.sopotnytska <olha.sopotnytska.pz.2020@lpnu.ua>
2022-12-16 16:52:23 +0200

    done

---
+++ SopotnytskaOlhaPZ35/Insert.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SopotnytskaOlhaPZ35
+{
+    /// <summary>
+    /// Interaction logic for Insert.xaml
+    /// </summary>
+    public partial class Insert : Window
+    {
+        public Insert()
+        {
+            InitializeComponent();
+        }
+        filmsEntities1 _db = new filmsEntities1();
+
+        private void insertBtn_Click(object sender, RoutedEventArgs e)
+        {
+            member newHotel = new member()
+            {
+                name = nameBox.Text,
+                director = directorBox.Text,
+                duration = durationBox.Text,
+                genre = genreBox.Text,
+                recommend=recommendBox.Text,
+                comment=commentBox.Text
+            };
+            _db.members.Add(newHotel);
+            _db.SaveChanges();
+            MainWindow.dataGrid.ItemsSource = _db.members.ToList();
+            this.Hide();
+        }
+    }
+}
---
+++ SopotnytskaOlhaPZ35/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SopotnytskaOlhaPZ35
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        filmsEntities1 _db = new filmsEntities1();
+        public static DataGrid dataGrid;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Load();
+        }
+        private void Load()
+        {
+            myDataGrid.ItemsSource = _db.members.ToList();
+            dataGrid = myDataGrid;
+        }
+
+        private void insertBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Insert iPage = new Insert();
+            iPage.ShowDialog();
+        }
+
+
+        private void deleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int Id = (myDataGrid.SelectedItem as member).id;
+            var deleteHotel = _db.members.Where(h => h.id == Id).Single();
+            _db.members.Remove(deleteHotel);
+
+            _db.SaveChanges();
+            dataGrid.ItemsSource = _db.members.ToList();
+            //this.Hide();
+        }
+
+    }
+}
+    
+
